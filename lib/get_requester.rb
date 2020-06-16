99ec71957d7413edd43895eb23aa25d03043b06f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-16 19:44:05 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -16,9 +16,9 @@ def get_response_body
 end
 
 def parse_json
-  programs = JSON.parse(self.get_programs)
+  @programs = JSON.parse(self.get_response_body)
   programs.collect do |program|
-  program["agency"]  
+  program["agency"]
 end
 
 end
