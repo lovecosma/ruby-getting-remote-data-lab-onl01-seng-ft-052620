3e732632a85749d93dad60615381715553173b4a
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-16 19:42:42 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,9 +6,9 @@ end
 
 
 def get_response_body
-  uri = URI.parse(URL)
-  response = Net::HTTP.get_response(uri)
-  response.body
+  @uri = URI.parse(@url)
+  @response = Net::HTTP.get_response(@uri)
+  @response.body
 end
 
 end
